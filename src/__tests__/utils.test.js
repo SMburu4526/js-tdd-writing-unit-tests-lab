392da0e803d848f5c0efc23d3c52e3a643b8e1a8
Stephen <stephenmburumburu82@gmail.com>
2025-07-28 14:48:21 +0300

    js-tdd-writing-unit-tests-lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
-// Your tests here
+const { isPalindrome } = require("../utils");
+
+describe("isPalindrome", () => {
+  test('returns true for "racecar"', () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  test('returns false for "car"', () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  test('returns true for mixed case "RaceCar"', () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  test("returns false for empty string", () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  describe("bonus tests", () => {
+    test("throws error for non-alphabetic characters", () => {
+      expect(() => isPalindrome("race car")).toThrow();
+    });
+
+    test("throws error for non-string input", () => {
+      expect(() => isPalindrome(123)).toThrow();
+    });
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
-// Your code here
+function isPalindrome(str) {
+  if (str === "") {
+    return false;
+  }
+
+  if (typeof str !== "string") {
+    throw new Error("Input must be a string");
+  }
+
+  if (!/^[A-Za-z]+$/.test(str)) {
+    throw new Error("Input must contain only alphabetic characters");
+  }
+
+  const lowerStr = str.toLowerCase();
+  return lowerStr === lowerStr.split("").reverse().join("");
+}
+
+module.exports = { isPalindrome };
